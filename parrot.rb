110f8140c84b18c6adf00f7a32314ad685863cc9
NancyKo <konancys@gmail.com>
2015-07-29 15:39:52 -0400

    finished parrot

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+talk = "Bark, bark, woof"
+
+def parrot(phrase="Squawk!")
+  puts phrase
+  parrot_says = phrase 
+end
+
+parrot(talk)
